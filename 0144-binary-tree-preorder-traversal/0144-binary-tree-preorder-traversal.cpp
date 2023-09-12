718d20e9a102aee6a1dd7ceb155ccbf7f4221b6c
SenaAbhishek <101617549+X-sena-X@users.noreply.github.com>
2023-09-12 11:22:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.03%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -25,7 +25,22 @@ public:
     vector<int> preorderTraversal(TreeNode* root) {
         
         if(!root) return ans;
-        helper(root);
+        //helper(root);
+        stack<TreeNode* > st;
+        st.push(root);
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+
+            if(root->right!=NULL){
+                st.push(root->right);
+            }
+            if(root->left!=NULL){
+                st.push(root->left);
+            }
+        }
+        
         return ans;
         
     }
