ca4f6d4c03e85eb9d45c1cf4e78c3c3d62a87b8f
Sena Abhishek <senaabhisheksvs@gmail.com>
2026-07-16 16:07:14 -0400

    Time: 0 ms (100.00%) | Memory: 20.5 MB (98.90%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> freq;
-
-        for(int i=0 ; i < nums.size() ; i++){
-            if(freq.find(nums[i]) == freq.end()){
-                freq[nums[i]] =1;
-            }
-            else freq[nums[i]]++;
+        int ans=0;
+        for(const auto& it:nums){
+            ans^=it;
         }
-        for(const auto& it:freq){
-            if(it.second==1) return it.first;
-        }
-
-        return 0;
+        return ans;
     }
 };
