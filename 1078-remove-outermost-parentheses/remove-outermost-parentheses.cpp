8f128b6ba4f7fd154ec22701414a059865fab03b
Sena Abhishek <senaabhisheksvs@gmail.com>
2026-07-26 02:31:54 -0400

    Time: 0 ms (100.00%) | Memory: 8.9 MB (81.64%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int opened=0;
+        for (const auto c : s){
+            if( c == '(' && opened++ > 0 ) ans += c;
+            if( c == ')' && opened-- > 1) ans += c;
+        }
+        return ans;
+    }
+};
