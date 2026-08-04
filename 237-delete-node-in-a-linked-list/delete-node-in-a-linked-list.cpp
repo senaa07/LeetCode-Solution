f951bffe504750e137850b035adcce0eaba4bced
Sena Abhishek <senaabhisheksvs@gmail.com>
2026-08-04 13:02:27 -0400

    Time: 10 ms (35.48%) | Memory: 12.5 MB (23.76%) - LeetSync

---
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *current = node;
+        ListNode *nextNode = current->next;
+        ListNode *nullNode = NULL;
+
+        while(nextNode->next != NULL){
+            current->val = nextNode->val;
+
+            current = current->next;
+            nextNode = nextNode->next;
+        }
+        current->val = nextNode->val;
+        current->next = NULL;
+        
+
+        return;
+    }
+};
